63271d24e16506c005535f68a740f262259b27b3
Sandeep Y S <sandy4uus2003@gmail.com>
2018-05-25 20:12:51 +0530

    removed listener reference

--- lib/redmine_webhook.rb
+++ lib/redmine_webhook.rb
@@ -2,7 +2,6 @@ require 'redmine_webhook/projects_helper_patch'
 require 'redmine_webhook/issue_patch'
 require 'redmine_webhook/journal_patch'
 require 'redmine_webhook/helper'
-require 'redmine_webhook/webhook_listener'
 
 module RedmineWebhook
 end
