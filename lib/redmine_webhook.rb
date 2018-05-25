197ee8d833b1956af5e387a503bb494846230ac5
Sandeep Y S <sandy4uus2003@gmail.com>
2018-05-25 17:05:05 +0530

    updated issue

--- lib/redmine_webhook.rb
+++ lib/redmine_webhook.rb
@@ -1,7 +1,6 @@
 require 'redmine_webhook/projects_helper_patch'
 require 'redmine_webhook/issue_patch'
 require 'redmine_webhook/journal_patch'
-require 'redmine_webhook/issue_wrapper'
 require 'redmine_webhook/helper'
 require 'redmine_webhook/webhook_listener'
 
